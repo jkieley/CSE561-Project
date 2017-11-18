beb2946f2433dfb22398b8cd5f15b8041e6169ea
bmathis23 <bamathis94@gmail.com>
2017-11-18 16:00:18 -0700

    Add Connection List and Node Connections

--- LoadBalancer/LoadBalancer.java
+++ LoadBalancer/LoadBalancer.java
@@ -1,76 +1,109 @@
 package LoadBalancer;
-
-import GenCol.Queue;
 import GenCol.entity;
 import model.modeling.message;
 import view.modeling.ViewableAtomic;
 
+import java.util.Vector;
 
-public class LoadBalancer extends ViewableAtomic {
+import GenCol.*;
+
+
+public class LoadBalancer extends ViewableAtomic 
+	{
 	protected entity job;
 	protected Queue nodes;
-
-	//=============================================================================================
-	public LoadBalancer(Queue nodeList) {
+	int[] nodeConnections;
+	
+//=============================================================================================
+	public LoadBalancer(Queue nodeList) 
+		{
 		super("LoadBalancer");
 		nodes = nodeList;
+		nodeConnections = new int[nodes.size()];
+		for(int i = 0; i < nodes.size(); i++)
+			nodeConnections[i] = (((Node)nodes.getFirst()).waitLength);
 		addInport("jobIn");
+		addInport("Connections");
 		addOutport("jobOut");
 		addTestInput("jobIn", new Job("job0", 5, 5, 10, true));
-	}
+		}
+//=============================================================================================
+		public void initialize() 
+			{
+			passivateIn("Waiting");
+			super.initialize();
+			}
+//=============================================================================================
+		public void deltext(double e, message x) 
+			{
+			Continue(e);
+			if (phaseIs("Waiting"))
+				{
+				for (int i = 0; i < x.getLength(); i++)
+					{
+					if (messageOnPort(x, "jobIn", i)) 
+						{	
+						job = x.getValOnPort("jobIn", i);
+						((Job)job).setNode(chooseNode());
+						holdIn("Delegating",0);
+						}
+					else if (messageOnPort(x, "Connections", i)) 
+						{
+						String processor;
+						Pair data = (Pair) x.getValOnPort("Connections", i);
+						processor = (String)data.key;
+						for(int j = 0; j < nodes.size(); j++)
+							{
+							Node temp = ((Node)nodes.get(j));
+							if(temp.getName().equals(processor))
+								nodeConnections[j] = (int)data.value;
+							}
 
-	//=============================================================================================
-	public void initialize() {
-		passivateIn("Waiting");
-		super.initialize();
-	}
-
-	//=============================================================================================
-	public void deltext(double e, message x) {
-		Continue(e);
-		if (phaseIs("Waiting")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "jobIn", i)) {
-					job = x.getValOnPort("jobIn", i);
-					((Job) job).setNode(chooseNode());
-					holdIn("Delegating", 0);
+						}
+					}
 				}
 			}
-		}
+//=============================================================================================
+		public String chooseNode() 
+			{
+			String node = "";
+			node = roundRobin();
+			return node;
+			}
+//=============================================================================================
+		public String roundRobin() 
+				{
+				Node node;
+				node = (Node) nodes.remove();	
+				nodes.add(node);
+				return node.getName();
+				}
+//=============================================================================================
+		public void deltint() 
+			{
+			passivateIn("Waiting");
+			}
+//=============================================================================================
+		public void deltcon(double e, message msg)
+			{
+			deltint ( );
+			deltext (0, msg);
+			}
+//=============================================================================================
+		public message out()
+			{
+			message m = new message();
+			m.add(makeContent("jobOut", job));
+			return m;
+			}
+//=============================================================================================
+		public void showState()
+			{
+			super.showState();
+			}
+//=============================================================================================
+		public String getTooltipText() 
+			{
+			return super.getTooltipText() + "\n" + job.getName();
+			}
 	}
-
-	//=============================================================================================
-	public String chooseNode() {
-		String node = "";
-
-		return node;
-	}
-
-	//=============================================================================================
-	public void deltint() {
-		passivateIn("Waiting");
-	}
-
-	//=============================================================================================
-	public void deltcon(double e, message msg) {
-		deltint();
-		deltext(0, msg);
-	}
-
-	//=============================================================================================
-	public message out() {
-		message m = new message();
-		m.add(makeContent("jobOut", job));
-		return m;
-	}
-
-	//=============================================================================================
-	public void showState() {
-		super.showState();
-	}
-
-	//=============================================================================================
-	public String getTooltipText() {
-		return super.getTooltipText() + "\n" + job.getName();
-	}
-}
--- LoadBalancer/Node.java
+++ LoadBalancer/Node.java
@@ -1,141 +1,166 @@
 package LoadBalancer;
-
-import GenCol.Queue;
 import GenCol.entity;
 import model.modeling.message;
 import view.modeling.ViewableAtomic;
+import GenCol.*;
 
 
-public class Node extends ViewableAtomic {
+public class Node extends ViewableAtomic 
+	{
 	protected entity job;
+	protected int waitLength = 5;
 	protected double CPUCapacity;
 	protected double MemoryCapacity;
 	protected double DBConnectionCapacity;
 	protected Queue connectionList;
-
-	//=============================================================================================
-	public Node(String name, double cpu, double memory, double dbConnections) {
+	protected Queue waitingList;
+	
+//=============================================================================================
+	public Node(String name, double cpu, double memory, double dbConnections) 
+		{
 		super(name);
 		CPUCapacity = cpu;
 		MemoryCapacity = memory;
 		DBConnectionCapacity = dbConnections;
 		connectionList = new Queue();
+		waitingList = new Queue();
 		addInport("jobIn");
 		addOutport("jobOut");
+		addOutport("Connections");
 		addTestInput("jobIn", new Job("job0", 5, 5, 10, true));
 		initialize();
-	}
-
-	//=============================================================================================
-	public void initialize() {
-		passivateIn("Waiting");
-		super.initialize();
-	}
-
-	//=============================================================================================
-	public void deltext(double e, message x) {
-		Continue(e);
-		if (phaseIs("Waiting")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "jobIn", i)) {
-					Job processJob = (Job) x.getValOnPort("jobIn", i);
-					if (processJob.getProcessor().equals(this.name)) {
-						job = x.getValOnPort("jobIn", i);
-						double cpuToUse = processJob.getCPUNeeded();
-						double memoryToUse = processJob.getMemoryNeeded();
-						int connection = 0;
-						if (processJob.isConnectionNeeded())
-							connection = 1;
-						CPUCapacity = CPUCapacity - cpuToUse;
-						MemoryCapacity = MemoryCapacity - memoryToUse;
-						DBConnectionCapacity = DBConnectionCapacity - connection;
-						connectionList.add(job);
-						holdIn("Working", processJob.getTimeNeeded());
-					}
-				}
+		}
+//=============================================================================================
+		public void initialize() 
+			{
+			passivateIn("Waiting");
+			super.initialize();
 			}
-		} else if (phaseIs("Working")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "jobIn", i)) {
-					Job processJob = (Job) x.getValOnPort("jobIn", i);
-					if (processJob.getProcessor().equals(this.name)) {
-						double cpuToUse = processJob.getCPUNeeded();
-						double memoryToUse = processJob.getMemoryNeeded();
-						int connection = 0;
-						if (processJob.isConnectionNeeded())
-							connection = 1;
-						if (CPUCapacity - cpuToUse >= 0 && MemoryCapacity - memoryToUse >= 0 && DBConnectionCapacity - connection >= 0) {
+//=============================================================================================
+		public void deltext(double e, message x) 
+			{
+			Continue(e);
+			if (phaseIs("Waiting"))
+				{
+				for (int i = 0; i < x.getLength(); i++)
+					{
+					if (messageOnPort(x, "jobIn", i)) 
+						{	
+						Job processJob = (Job)x.getValOnPort("jobIn", i);
+						if(processJob.getProcessor().equals(this.name))
+							{
+							job = x.getValOnPort("jobIn", i);
+							double cpuToUse = processJob.getCPUNeeded();
+							double memoryToUse = processJob.getMemoryNeeded();
+							int connection = 0;
+							if(processJob.isConnectionNeeded())
+								connection = 1;
 							CPUCapacity = CPUCapacity - cpuToUse;
 							MemoryCapacity = MemoryCapacity - memoryToUse;
 							DBConnectionCapacity = DBConnectionCapacity - connection;
-							connectionList.add(processJob);
-							int size = connectionList.size();
-							double min = Double.MAX_VALUE;
-							int index = 0;
-							for (int j = 0; j < size; j++) {
-								Job temp = (Job) connectionList.get(j);
-								temp.ProcessingTime = temp.ProcessingTime - e;
-								if (temp.ProcessingTime <= min) {
-									job = temp;
-									min = temp.ProcessingTime;
-								}
+							connectionList.add(job);
+							holdIn("Working", processJob.getTimeNeeded());
 							}
-							holdIn("Working", ((Job) job).getTimeNeeded());
+						}
+					}
+				}
+			else if (phaseIs("Working"))
+				{
+				for (int i = 0; i < x.getLength(); i++)
+					{
+					if (messageOnPort(x, "jobIn", i)) 
+						{
+						Job processJob = (Job)x.getValOnPort("jobIn", i);
+						if(processJob.getProcessor().equals(this.name))
+							{
+							double cpuToUse = processJob.getCPUNeeded();
+							double memoryToUse = processJob.getMemoryNeeded();
+							int connection = 0;
+							if(processJob.isConnectionNeeded())
+								connection = 1;
+							if(CPUCapacity - cpuToUse >= 0 && MemoryCapacity - memoryToUse >= 0 && DBConnectionCapacity - connection >= 0)
+								{
+								CPUCapacity = CPUCapacity - cpuToUse;
+								MemoryCapacity = MemoryCapacity - memoryToUse;
+								DBConnectionCapacity = DBConnectionCapacity - connection;
+								connectionList.add(processJob);
+								int size = connectionList.size();
+								double min = Double.MAX_VALUE;
+								int index = 0;
+								for(int j = 0; j < size; j++)
+									{
+									Job temp = (Job)connectionList.get(j);
+									temp.ProcessingTime = temp.ProcessingTime - e;
+									if(temp.ProcessingTime <= min)
+										{
+										job = temp;
+										min = temp.ProcessingTime;
+										}	
+									}
+								holdIn("Working", ((Job)job).getTimeNeeded());
+								}
+							else if(waitingList.size() < waitLength)
+								waitingList.add(processJob);
+							}
 						}
 					}
 				}
 			}
-		}
-	}
-
-	//=============================================================================================
-	public void deltint() {
-		Job processJob = (Job) connectionList.remove(connectionList.indexOf(job));
-		double cpuUsed = processJob.getCPUNeeded();
-		double memoryUsed = processJob.getMemoryNeeded();
-		int connection = 0;
-		if (processJob.isConnectionNeeded())
-			connection = 1;
-		CPUCapacity = CPUCapacity + cpuUsed;
-		MemoryCapacity = MemoryCapacity + memoryUsed;
-		DBConnectionCapacity = DBConnectionCapacity + connection;
-		if (connectionList.isEmpty())
-			passivateIn("Waiting");
-		else {
-			int size = connectionList.size();
-			double min = Double.MAX_VALUE;
-			for (int j = 0; j < size; j++) {
-				Job temp = (Job) connectionList.get(j);
-				temp.ProcessingTime = temp.ProcessingTime - processJob.ProcessingTime;
-				if (temp.ProcessingTime <= min) {
-					job = temp;
-					min = temp.ProcessingTime;
+//=============================================================================================
+		public void deltint() 
+			{
+			Job processJob = (Job)connectionList.remove(connectionList.indexOf(job));
+			double cpuUsed = processJob.getCPUNeeded();
+			double memoryUsed = processJob.getMemoryNeeded();
+			int connection = 0;
+			if(processJob.isConnectionNeeded())
+				connection = 1;
+			CPUCapacity = CPUCapacity + cpuUsed;
+			MemoryCapacity = MemoryCapacity + memoryUsed;
+			DBConnectionCapacity = DBConnectionCapacity + connection;
+			if(waitingList.size() > 0)
+				connectionList.add(waitingList.removeFirst());
+			if(connectionList.isEmpty())
+				passivateIn("Waiting");
+			else
+				{
+				int size = connectionList.size();
+				double min = Double.MAX_VALUE;
+				for(int j = 0; j < size; j++)
+					{
+					Job temp = (Job)connectionList.get(j);
+					temp.ProcessingTime = temp.ProcessingTime - processJob.ProcessingTime;
+					if(temp.ProcessingTime <= min)
+						{
+						job = temp;
+						min = temp.ProcessingTime;
+						}	
+					}
+				holdIn("Working", ((Job)job).getTimeNeeded());
 				}
 			}
-			holdIn("Working", ((Job) job).getTimeNeeded());
-		}
+//=============================================================================================
+		public void deltcon(double e, message msg)
+			{
+			deltint ( );
+			deltext (0, msg);
+			}
+//=============================================================================================
+		public message out()
+			{
+			message m = new message();
+			m.add(makeContent("jobOut", job));
+			m.add(makeContent("Connections", new Pair(name ,waitLength - waitingList.size())));
+			return m;
+			}
+//=============================================================================================
+		public void showState()
+			{
+			super.showState();
+			}
+//=============================================================================================
+		public String getTooltipText() 
+			{
+			return super.getTooltipText() + "\n" + job.getName();
+			}
 	}
-
-	//=============================================================================================
-	public void deltcon(double e, message msg) {
-		deltint();
-		deltext(0, msg);
-	}
-
-	//=============================================================================================
-	public message out() {
-		message m = new message();
-		m.add(makeContent("jobOut", job));
-		return m;
-	}
-
-	//=============================================================================================
-	public void showState() {
-		super.showState();
-	}
-
-	//=============================================================================================
-	public String getTooltipText() {
-		return super.getTooltipText() + "\n" + job.getName();
-	}
-}
